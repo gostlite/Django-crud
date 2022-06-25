c0012153cda71a3d5d3354414b6f726a2652a263
User <foodhubnig@gmail.com>
2022-06-25 23:16:48 +0100

    m

---
+++ blog/views.py
@@ -0,0 +1,38 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.urls import reverse_lazy
+from .models import Post
+# Create your views here.
+
+class PostListView(ListView):
+    model= Post
+    template_name= "post_list.html"
+
+
+class PostCreateView(CreateView):
+    model= Post
+    fields= "__all__"
+    template_name= "post_form.html"
+    success_url= reverse_lazy("blog:all")
+    
+
+
+class PostDetailView(DetailView):
+    model= Post
+    template_name= "post_detail.html"
+    
+
+class PostUpdateView(UpdateView):
+    model= Post
+    fields="__all__"
+    template_name= "post_form.html"
+    success_url= reverse_lazy("blog:all")
+    
+
+class PostDeleteView(DeleteView):
+    model= Post
+    fields="__all__"
+    template_name= "../templates/post_confirm_delete.html"
+    success_url= reverse_lazy("blog:all")
+
